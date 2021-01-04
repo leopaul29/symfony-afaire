ea024feb240ed974626c2da1ec588d52944c9184
Leo-paul <leopaulmartin@hotmail.fr>
2021-01-04 23:04:51 +0100

    first page hello world

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\StringUtils;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/hello")
+ */
+class HelloController extends AbstractController {
+    
+/**
+ * @Route("/{name}")
+ */
+    public function world(StringUtils $stringUtils, $name) {
+        $name = $stringUtils->capitalize($name);
+        return $this->render('hello.html.twig', ['name'=>$name]);
+    }
+}
