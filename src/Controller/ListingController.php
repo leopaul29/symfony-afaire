ae637b7a321c94839231f1c3fc058fd8826cde56
Leo-paul <leopaulmartin@hotmail.fr>
2021-01-05 23:35:40 +0100

    feature: listing and create tasks

--- src/Controller/ListingController.php
+++ src/Controller/ListingController.php
@@ -6,9 +6,11 @@ use App\Entity\Listing;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
- * @Route("/")
+ * @Route("/", name="listing_")
  */
 class ListingController extends AbstractController {
     
@@ -19,5 +21,18 @@ class ListingController extends AbstractController {
         $listings = $entityManager->getRepository(Listing::class)->findAll();
         
         return $this->render('listing.html.twig', ['listings'=>$listings]);
+    }
+
+    /**
+     * @Route("/new", method="POST", name="create")
+     */
+    public function create(EntityManagerInterface $entityManager, Request $request) {
+        $name = $request->get('name');
+        $listing = new Listing();
+
+        $entityManager->persist($listing);
+        $entityManager->flush();
+
+        return new Response("OK");
     }
 }
